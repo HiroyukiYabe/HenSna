17cb0bba5bcc819bc17c77b304b5eeb1065d519f
Hiroyuki Yabe <yabe@cyber.t.u-tokyo.ac.jp>
2014-09-19 15:08:08 +0900

    Git管理に合わせプロジェクトを修正

---
+++ henSna/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,46 @@
+﻿//スワイプ量を取得し、カメラを移動・回転
+
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public float MoveSpeed = 5.0f;
+	public float RotSpeed = 20.0f;
+	public float LimitRotX = 60.0f;
+	TapDetect tapDetect;
+	CharacterController charCon;
+	//GameObject player;
+	
+	// Use this for initialization
+	void Start () {
+		tapDetect = GameObject.Find ("GameController").GetComponent<TapDetect> ();
+		charCon = GetComponent<CharacterController>();
+		//player = transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 rot = tapDetect.UpDrag ();
+		Vector2 move = tapDetect.DownDrag ();
+
+		//スワイプ量を元に前後左右に移動
+		Vector3 nowForward = new Vector3(transform.forward.x,0,transform.forward.z).normalized;
+		Vector3 nowRight = new Vector3(transform.right.x,0,transform.right.z).normalized;
+		charCon.Move( (nowRight*move.x + nowForward*move.y) * MoveSpeed*Time.deltaTime);
+
+		//スワイプ量を元に回転
+		float _rotX = transform.eulerAngles.x - rot.y * RotSpeed *Time.deltaTime;
+		float _rotY = transform.eulerAngles.y + rot.x * RotSpeed *Time.deltaTime;
+		//x方向の回転を制限  回転可能角度外
+		if(_rotX > LimitRotX && _rotX < 360f - LimitRotX){
+			//下と上のどちらから可能角度を超えたか それに応じて制限
+			_rotX = _rotX > 180f ? 360f - LimitRotX : LimitRotX;
+		}
+		//回転
+		transform.rotation = Quaternion.Euler(_rotX,_rotY,transform.eulerAngles.z);
+		
+	}
+
+}
---
+++ henSna/Assets/Scripts/TapDetect.cs
@@ -0,0 +1,103 @@
+﻿// タップ位置、スワイプ量を取得
+
+
+using UnityEngine;
+using System.Collections;
+
+public class TapDetect : MonoBehaviour {
+
+	Vector2 downStartPos;
+	Vector2 upStartPos;
+	Vector2 downMove;
+	Vector2 upMove;
+	bool isDownTouch;
+	bool isUpTouch;
+	int downID;
+	int upID;
+
+
+	// Use this for initialization
+	void Start () {
+		isDownTouch = false;
+		isUpTouch = false;
+		downMove = Vector2.zero;
+		upMove = Vector2.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.touchCount > 0) {
+			foreach(Touch touch in Input.touches){
+				switch(touch.phase){
+				case TouchPhase.Began:
+					if(!isDownTouch && touch.position.y <= Screen.height/2){
+						isDownTouch = true;
+						downStartPos = touch.position;
+						downID = touch.fingerId;
+					}else if(!isUpTouch && touch.position.y > Screen.height/2){
+						isUpTouch = true;
+						upStartPos = touch.position;
+						upID = touch.fingerId;
+					}
+					break;
+				case TouchPhase.Moved:
+					if (isDownTouch && touch.fingerId==downID)	downMove = (touch.position - downStartPos);
+					else if (isUpTouch && touch.fingerId==upID)	upMove = (touch.position - upStartPos);
+					break;
+				case TouchPhase.Ended:
+					if (isDownTouch && touch.fingerId==downID){
+						downMove = Vector2.zero;
+						isDownTouch = false;
+						downID = -1;
+					}
+					else if (isUpTouch && touch.fingerId==upID){
+						upMove = Vector2.zero;
+						isUpTouch = false;
+						upID = -1;
+					}
+					break;
+				case TouchPhase.Canceled:
+					break;
+					if (isDownTouch && touch.fingerId==downID){
+						downMove = Vector2.zero;
+						isDownTouch = false;
+						downID = -1;
+					}
+					else if (isUpTouch && touch.fingerId==upID){
+						upMove = Vector2.zero;
+						isUpTouch = false;
+						upID = -1;
+					}
+				}
+			}
+		}
+
+	}
+
+
+
+	//Get drag movement in upper area
+	public Vector2 UpDrag(){
+		float resx = upMove.x/(Screen.height/2);
+		if (Mathf.Abs (resx) > 1)
+			resx = Mathf.Sign(resx);
+		float resy = upMove.y/(Screen.height/2);
+		if (Mathf.Abs (resy) > 1)
+			resy = Mathf.Sign(resy);
+		return new Vector2(resx,resy);
+	}
+
+	//Get drag movement in lower area
+	public Vector2 DownDrag(){
+		float resx = downMove.x/(Screen.height/2);
+		if (Mathf.Abs (resx) > 1)
+			resx = Mathf.Sign(resx);
+		float resy = downMove.y/(Screen.height/2);
+		if (Mathf.Abs (resy) > 1)
+			resy = Mathf.Sign(resy);
+		return new Vector2(resx,resy);
+	}
+
+
+}
