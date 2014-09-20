a0fbbbef656e58f0da688161738a5ece5ede9759
Hara <hara.k.aj@m.titech.ac.jp>
2014-09-20 23:50:13 +0900

    ダンジョン選択ページレイアウト、遷移先実装

---
+++ henSna/Assets/Scripts/selectDungeon/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+				Debug.Log ("come start select dungeon");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				Debug.Log ("come update22");
+	}
+
+		public void clickJapaneseDungeon(){
+				Application.LoadLevel ("Field1_Japan");
+		}
+
+		public void clickBackButton(){
+				Application.LoadLevel ("home");
+		}
+}
